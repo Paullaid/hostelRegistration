2f11d14e0d01cc9a74b86676cae1e05daf8902d2
Ngubi Paul Fomonyuy <ngubipaul@gmail.com>
2025-08-09 08:05:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className=" container mx-auto text-lg bg-blue-400 font-bold">Hello World Fuck ya</div>
+    
+}
+
+export default App
